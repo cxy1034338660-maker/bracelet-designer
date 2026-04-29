6a265f7f681854bdaf97cc16148b2500ef5d2484
cxy1034338660-maker <cxy1034338660@gmail.com>
2026-04-29 20:17:55 +0800

    deploy github pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/bracelet-designer/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
